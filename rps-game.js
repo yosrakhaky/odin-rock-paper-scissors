6ac7458d27040dcf58b7c6570f6e7d15b6bf7a37
yosrakhaky <yosrakhaky@gmail.com>
2022-06-30 15:32:24 -0400

    adjust computerSelection choices to lowercase

--- rps-game.js
+++ rps-game.js
@@ -2,11 +2,11 @@ function computerPlay () {
     const max = 3, min = 1;
     RandomSelection = Math.floor(Math.random() * (max - min + 1) + min)
     if (RandomSelection == 1) {
-        computerSelection = "Rock";
+        computerSelection = "rock";
     } else if (RandomSelection == 2) {
-        computerSelection = "Paper";
+        computerSelection = "paper";
     }else if (RandomSelection == 3) {
-        computerSelection = "Scissors"}
+        computerSelection = "scissors"}
 
     return computerSelection
 }
