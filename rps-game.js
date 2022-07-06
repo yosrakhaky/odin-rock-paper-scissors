79092565c3e9e4d28a8d122e853dbb122a1c634d
yosrakhaky <yosrakhaky@gmail.com>
2022-07-06 16:54:07 -0400

    add all scenario combinations to playRound

--- rps-game.js
+++ rps-game.js
@@ -16,5 +16,18 @@ playerSelection = prompt("rock, paper, or scissors?")
 function playRound(playerSelection, computerSelection) {
     if (playerSelection.toLowerCase() == computerSelection) {
         alert("Draw, play again!");
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "paper") {
+        alert("You Lose! Paper beats Rock");
+    } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "scissors") {
+        alert("You win! Rock beats Scissors!")
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "rock") {
+        alert("You won! Paper beats Rock!")
+    } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "scissors") {
+        alert("You Lose! Scissors beat Paper")
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "rock") {
+        alert("You Lose! Rock beats Scissors")
+    } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "paper") {
+        alert("You Win! Scissors beat Paper")
     }
 }
+
